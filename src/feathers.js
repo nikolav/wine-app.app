3bad4d6b729d5b461aed7e80955135ce72cabf12
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-04 09:35:56 +0200

    push.heroku

--- src/feathers.js
+++ src/feathers.js
@@ -2,8 +2,8 @@ import feathers from "@feathersjs/client";
 import socketio from "@feathersjs/socketio-client";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3031");
-// const socket = io("https://wine-app-api.herokuapp.com/");
+// const socket = io("http://localhost:3031");
+const socket = io("https://wine-app-api.herokuapp.com/");
 const client = feathers();
 
 client.configure(socketio(socket));
