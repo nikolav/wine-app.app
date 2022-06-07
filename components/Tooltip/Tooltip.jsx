22c0b0c7ce02dfc783cba0ab6ecdd7e40bf5b59a
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-07 16:56:07 +0200

    tooltip gradient

--- components/Tooltip/Tooltip.jsx
+++ components/Tooltip/Tooltip.jsx
@@ -55,7 +55,7 @@ export default function Tooltip({
               initial={{ opacity: 0, y: 2 }}
               exit={{ opacity: 0, transition: { duration: 0.1 } }}
               animate={{ opacity: 1, y: 0, transition: { duration: 0.24 } }}
-              className={`bg-slate-900 p-2 text-slate-200 shadow rounded text-xs ${classes}`}
+              className={`bg-gradient-to-b from-slate-800 to-slate-900 p-2 text-slate-200 shadow rounded text-xs ${classes}`}
             >
               {/*  */}
               {/* tooltip content */}
