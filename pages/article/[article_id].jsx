21b261ee72da785a7c3db81decad7c881b51b3e3
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-07 11:57:42 +0200

    blank post previews

---
+++ pages/article/[article_id].jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//
+const PreviewArticle = () => {
+  return <div>@todo; article.preview</div>;
+};
+
+//
+export default PreviewArticle;
---
+++ pages/winereview/[winereview_id].jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//
+const PreviewWineReview = () => {
+  return <div>@todo; winereview.preview</div>;
+};
+
+//
+export default PreviewWineReview;
