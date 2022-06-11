c2618aa4bd868367d2fd5d6d0591b87aca7d9ad8
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-11 19:36:23 +0200

    home button @article.preview

--- pages/article/[article_id].jsx
+++ pages/article/[article_id].jsx
@@ -1,63 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useArticles } from "../../app/store";
 import { CommentsLike } from "../../components/social";
-//
 import Image from "next/image";
 import imagePlaceholder from "../../public/placeholder01.png";
 import { bgArticleImage } from "./Article.module.css";
 import { slateSerialize } from "../../components/SlateEditable/SlateEditable";
+import PortalOverlays from "../../components/PortalOverlays";
+import { FaHome } from "../../components/icons";
+import Tooltip from "../../components/Tooltip/Tooltip";
+import useStateSwitch from "../../src/hooks/use-state-switch";
 //
 const PreviewArticle = () => {
   const router = useRouter();
   const { article_id: ID } = router.query;
   const { articles } = useArticles();
   const article = (articles ?? []).find((a) => ID === a._id);
-
+  //
+  const [refPopperHome, setRefPopperHome] = useState(null);
+  const { isOn: isActiveHome, toggle: toggleIsActiveHome } = useStateSwitch();
+  const navigateHome = () => router.push("/");
+  //
   return (
-    <div className="h-screen overflow-y-auto scrollbar-thin">
-      <div
-        style={{
-          gridTemplateColumns: "58% auto",
-        }}
-        className="flex flex-col lg:flex-none lg:!grid h-full lg:overflow-hidden"
-      >
-        <section className="lg:overflow-y-auto scrollbar-thin">
-          <div className="sm:pt-4 md:pt-8">
-            <article className="prose min-h-screen mx-auto bg-white p-6 sm:rounded-t-2xl shadow-lg">
-              {article ? (
-                <>
-                  <h1 className="pb-2 italic text-slate-900/40 text-center md:text-right lg:pr-4 border-b-2 border-b-slate-900/40">{article.title}</h1>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: slateSerialize(JSON.parse(article.body)),
-                    }}
-                  />
-                </>
-              ) : null}
-            </article>
-          </div>
-        </section>
-        <section
-          className={`${bgArticleImage} lg:shadow-lg lg:border-l-4 lg:border-l-white order-first lg:order-none min-h-screen relative`}
+    <>
+      <PortalOverlays>
+        <span
+          ref={setRefPopperHome}
+          onMouseOver={toggleIsActiveHome.on}
+          onMouseLeave={toggleIsActiveHome.off}
+          className="fixed z-10 top-2 left-[50%] text-5xl text-slate-900"
         >
-          <Image
-            alt=""
-            layout="fill"
-            src={article?.image ?? imagePlaceholder.src}
-            className="object-cover object-center block"
+          <FaHome
+            onClick={navigateHome}
+            className="cursor-pointer opacity-40 hover:opacity-60 hover:scale-110 transition-transform duration-75"
           />
-          {null != article ? (
-            <CommentsLike
-              size="!sm"
-              id={`article--${article._id}`}
-              className="absolute top-2 left-2"
-              placement="bottom-end"
+        </span>
+        <Tooltip
+          offset={[0, 12]}
+          refElement={refPopperHome}
+          isActive={isActiveHome}
+          placement="bottom"
+        >
+          🏡 nazad na početnu stranu
+        </Tooltip>
+      </PortalOverlays>
+      <div className="h-screen overflow-y-auto scrollbar-thin">
+        <div
+          style={{
+            gridTemplateColumns: "55% auto",
+          }}
+          className="flex flex-col lg:flex-none lg:!grid h-full lg:overflow-hidden"
+        >
+          <section className="lg:overflow-y-auto scrollbar-thin">
+            <div className="sm:pt-4 md:pt-8">
+              <article className="prose min-h-screen mx-auto bg-white p-6 sm:rounded-t-2xl shadow-lg">
+                {article ? (
+                  <>
+                    <h1 className="pb-2 italic text-slate-900/40 text-center md:text-right lg:pr-4 border-b-2 border-b-slate-900/40">
+                      {article.title}
+                    </h1>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: slateSerialize(JSON.parse(article.body)),
+                      }}
+                    />
+                  </>
+                ) : null}
+              </article>
+            </div>
+          </section>
+          <section
+            className={`${bgArticleImage} lg:shadow-lg lg:border-l-4 lg:border-l-white order-first lg:order-none min-h-screen relative`}
+          >
+            <Image
+              alt=""
+              layout="fill"
+              src={article?.image ?? imagePlaceholder.src}
+              className="object-cover object-center block"
             />
-          ) : null}
-        </section>
+            {null != article ? (
+              <CommentsLike
+                size="!sm"
+                id={`article--${article._id}`}
+                className="absolute top-2 left-2"
+                placement="bottom-end"
+              />
+            ) : null}
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
