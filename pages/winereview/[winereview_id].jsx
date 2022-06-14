7ee611830278e859d928584bfa1c0a553d25d739
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-15 00:45:52 +0200

    @6

--- pages/winereview/[winereview_id].jsx
+++ pages/winereview/[winereview_id].jsx
@@ -8,16 +8,25 @@ const PreviewWineReview = () => {
   const { winereview_id: ID } = router.query;
   const globals = useGlobals();
   const { winereview } = useWineReview();
-  const wr = winereview?.find((node) => ID === node._id);
+  // const wr = winereview?.find((node) => ID === node._id);
   //
   //
+  // useEffect(() => {
+  //   console.log(ID)
+  //   console.log(wr)
+  //   console.log(winereview)
+  //   globals.set(WR_IS_PREVIEW, wr);
+  //   router.push(`/`);
+  // }, []);
   useEffect(() => {
-    console.log(ID)
-    console.log(wr)
-    console.log(winereview)
-    globals.set(WR_IS_PREVIEW, wr);
-    router.push(`/`);
-  }, []);
+    if (winereview) {
+      globals.set(
+        WR_IS_PREVIEW,
+        winereview.find((node) => ID === node._id)
+      );
+      router.push(`/`);
+    }
+  }, [winereview, ID, router]);
   //
   //https://wine-app0.herokuapp.com/winereview/62a90650e7901336c3981c1b
   return null;
