dd989b4d5e49bd8970de347eadc08b491fa27931
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-21 16:48:25 +0200

    auth#13

--- components/GuestNavigation/GuestNavigation.jsx
+++ components/GuestNavigation/GuestNavigation.jsx
@@ -57,7 +57,7 @@ const GuestNavigation = ({ ...rest }) => {
   //   : noop;
   const logout_ = () => {
     logout();
-    signOut({ redirect: false });
+    signOut();
   };
   //
   const { toggle } = useFlags();
